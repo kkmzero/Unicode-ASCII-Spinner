53a384ddca3aa4bc8518dac24302d678560da7dd
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2020-08-20 21:46:47 +0200

    Create main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unistd.h>
+
+char spinner_a[5] = { '\\', '|', '/', '-', '\0'};
+char spinner_b[7] = {'.', 'o', 'O', '@', '*', ' ', '\0'};
+char spinner_c[3] = {'+', 'x', '\0'};
+char spinner_d[5] = {'v', '<', '^', '>', '\0'};
+
+void run_spinner(char *spinner, int spinner_size, unsigned int sleeptime) {
+    while(1) {
+        for(int i = 0; i < spinner_size; i++) {
+            std::cout << "\b" << spinner[i] << std::flush;
+            usleep(sleeptime);
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    run_spinner(spinner_a, (sizeof(spinner_a) - sizeof(*spinner_a)), 200000);
+
+    return 0;
+}
