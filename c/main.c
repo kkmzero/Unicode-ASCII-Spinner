d8b1f392134e3ca757c12784b4b67f44c9681977
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2020-11-26 00:09:52 +0100

    Update main.c

--- c/main.c
+++ c/main.c
@@ -1,11 +1,8 @@
 /*
- * I dedicate any and all copyright interest in this software to the
- * public domain. I make this dedication for the benefit of the public at
- * large and to the detriment of my heirs and successors. I intend this
- * dedication to be an overt act of relinquishment in perpetuity of all
- * present and future rights to this software under copyright law.
+ * Demo code for unicode/ascii spinners.
  *
- * For more information, please refer to <http://unlicense.org/>
+ * CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+ * https://creativecommons.org/publicdomain/zero/1.0/
  */
 
 #include <stdio.h>
